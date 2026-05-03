57bc26fa60912b403a35fae2f93dff70faebf71d
fonetkz <fonetkz3@gmail.com>
2026-05-03 13:19:44 -0300

    feat: register /impressao route and add placeholder page

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import LoginPage from './pages/LoginPage'
 import DashboardPage from './pages/DashboardPage'
 import MembersPage from './pages/MembersPage'
 import ProgrammingPage from './pages/ProgrammingPage'
+import HymnPrintPage from './pages/HymnPrintPage'
 import SettingsPage from './pages/SettingsPage'
 import AccountSettingsPage from './pages/AccountSettingsPage'
-
 import ResetPasswordPage from './pages/ResetPasswordPage'
 import Toast from './components/ui/Toast'
 
@@ -15,20 +15,18 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Rota pública */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/redefinir-senha" element={<ResetPasswordPage />} />
 
-        {/* Rotas protegidas */}
         <Route element={<AuthLayout />}>
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/membros" element={<MembersPage />} />
           <Route path="/programacao" element={<ProgrammingPage />} />
+          <Route path="/impressao" element={<HymnPrintPage />} />
           <Route path="/configuracoes" element={<SettingsPage />} />
           <Route path="/conta" element={<AccountSettingsPage />} />
         </Route>
 
-        {/* Redirect padrão */}
         <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
 
